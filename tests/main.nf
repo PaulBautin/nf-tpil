289709a4c889e81b31824b47eb57b07514297853
PaulBautin <pbautin70@gmail.com>
2024-02-06 17:20:47 -0500

    add variable for output of tractoflow

--- tests/main.nf
+++ tests/main.nf
@@ -4,7 +4,7 @@ nextflow.enable.dsl=2
 include { SEGMENTATION_FSLFIRST } from '../modules/nf-core/segmentation/fslfirst/main'
 
 workflow {
-	input_tractoflow = file('/home/pabaua/dev_tpil/results/results_tracto/23-09-01_tractoflow_bundling')
+	input_tractoflow = file(params.input_tr)
 	
 	t1_nativepro_brain = Channel.fromPath("$input_tractoflow/*/Crop_T1/*__t1_bet_cropped.nii.gz").map{[[ id:it.parent.parent.name, single_end:false ], it]}
 	t1_nativepro_brain.view()
--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-my_main_nf='/home/pabaua/dev_tpil/nf-tpil/tests/main.nf'
 
-nextflow run $my_main_nf -work-dir ../results --outdir ../results/results -resume
+# main.nf to test nf-tpil
+my_main_nf='/home/pabaua/dev_tpil/nf-tpil/tests/main.nf'
+# Results folder of tractoflow
+my_input_tr='/home/pabaua/dev_tpil/results/results_tracto/23-09-01_tractoflow_bundling'
+
+nextflow run $my_main_nf \
+    --input_tr $my_input_tr \
+    -work-dir ../results \
+    --outdir ../results/results \
+    -resume
